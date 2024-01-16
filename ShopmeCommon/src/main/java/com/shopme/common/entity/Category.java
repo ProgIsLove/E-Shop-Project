f00282b60fd9e242dc6fa4ece894001ce761b08f
Tomáš Pardygal <58989540+ProgIsLove@users.noreply.github.com>
2024-01-16 23:52:13 +0100

    created new entity for category with repository, changed dependency versions, added equals and hashcode (#12)

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -0,0 +1,31 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+@Setter
+@Getter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(length = 128, nullable = false, unique = true)
+    private String name;
+    @Column(length = 64, nullable = false, unique = true)
+    private String alias;
+    @Column(length = 128, nullable = false, unique = true)
+    private String image;
+    private boolean enabled;
+    @OneToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> children = new HashSet<>();
+}
