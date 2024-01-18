b3cff97bc0fdcc48cdc94ee03a9f633f405f723e
Tomáš Pardygal <58989540+ProgIsLove@users.noreply.github.com>
2024-01-18 23:05:29 +0100

    added testDb to docker-compose, refactor docker-compose that can work with multiple database, refactor application.properties, added application-test.properties (#13)

--- ShopmeWeb/ShopmeBE/src/main/resources/sql/users.sql
+++ ShopmeWeb/ShopmeBE/src/main/resources/sql/users.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (first_name, last_name, password, email, photos, enabled)
-VALUES ('John', 'Croft', 'john123', 'john@shopme.com', null, true);
-
-INSERT INTO users (first_name, last_name, password, email, photos, enabled)
-VALUES ('Anna', 'Cute', 'password123', 'anna@shopme.com', null, false);
-
+# INSERT INTO users (first_name, last_name, password, email, photos, enabled)
+# VALUES ('John', 'Croft', 'john123', 'john@shopme.com', null, true);
+#
+# INSERT INTO users (first_name, last_name, password, email, photos, enabled)
+# VALUES ('Anna', 'Cute', 'password123', 'anna@shopme.com', null, false);
+#
