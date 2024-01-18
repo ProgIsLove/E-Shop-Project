a9ba97e6c20f66cf2f8b0fbfc8fd53f8853cf153
Tomáš <tomas.pardygal@seznam.cz>
2024-01-18 23:05:08 +0100

    added testDb to docker-compose, refactor docker-compose that can work with multiple database, refactor application.properties, added application-test.properties

--- ShopmeWeb/ShopmeBE/src/main/resources/sql/users_roles.sql
+++ ShopmeWeb/ShopmeBE/src/main/resources/sql/users_roles.sql
@@ -1,3 +1,3 @@
-INSERT INTO users_roles (user_id, role_id) VALUES (3, 1);
-INSERT INTO users_roles (user_id, role_id) VALUES (4, 3);
-INSERT INTO users_roles (user_id, role_id) VALUES (4, 2);
+# INSERT INTO users_roles (user_id, role_id) VALUES (3, 1);
+# INSERT INTO users_roles (user_id, role_id) VALUES (4, 3);
+# INSERT INTO users_roles (user_id, role_id) VALUES (4, 2);
