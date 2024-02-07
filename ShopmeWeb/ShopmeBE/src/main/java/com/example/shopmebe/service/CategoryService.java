9e1ce513a73c71a08b934d6be30e3fcae09b0c8b
Tomáš <tomas.pardygal@seznam.cz>
2024-02-07 23:49:38 +0100

    improve category hierarchy

--- ShopmeWeb/ShopmeBE/src/main/java/com/example/shopmebe/service/CategoryService.java
+++ ShopmeWeb/ShopmeBE/src/main/java/com/example/shopmebe/service/CategoryService.java
@@ -22,33 +22,21 @@ public class CategoryService {
     public List<Category> listCategoriesUsedInForm() {
         List<Category> categoriesUsedInForm = new ArrayList<>();
         Iterable<Category> categories = repo.findAll();
-        StringBuilder sb = new StringBuilder();
 
         for(Category category : categories) {
             if (category.getParent() == null) {
                 categoriesUsedInForm.add(Category.builder()
                         .name(category.getName()).build());
-                Set<Category> children = category.getChildren();
 
-                for(Category subCategory : children) {
-                    sb.delete(0, sb.length());
-                    sb.append("--");
-                    sb.append(subCategory.getName());
-
-                    categoriesUsedInForm.add(Category.builder()
-                            .name(sb.toString()).build());
-                    listChildren(categoriesUsedInForm, subCategory, 1);
-                }
+                listChildren(categoriesUsedInForm, category.getChildren(), 0);
             }
         }
-
 
         return categoriesUsedInForm;
     }
 
-    private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+    private void listChildren(List<Category> categoriesUsedInForm, Set<Category> children, int subLevel) {
         int newSubLevel = subLevel + 1;
-        Set<Category> children = parent.getChildren();
         StringBuilder sb = new StringBuilder();
 
         for (Category subCategory : children) {
@@ -57,7 +45,7 @@ public class CategoryService {
 
             categoriesUsedInForm.add(Category.builder()
                     .name(sb + subCategory.getName()).build());
-            listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+            listChildren(categoriesUsedInForm, subCategory.getChildren(), newSubLevel);
         }
     }
 }
