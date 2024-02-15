8bc20019610833edcdac87a0599ce2cb5a8fdf08
Tomáš <tomas.pardygal@seznam.cz>
2024-02-15 22:33:41 +0100

    added css, fixed no image showing, modified fragment syntax

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -72,8 +72,8 @@ public class Category {
 
     @Transient
     public String getImagePath() {
-        if (this.id == null || this.image == null) return "/images/image-preview-thumbnail.png";
-        return "../category-images/" + this.id + "/" + this.image;
+        if (this.id == null) return "/images/image-thumbnail.png";
+        return "/category-images/" + this.id + "/" + this.image;
     }
 
     @Override
