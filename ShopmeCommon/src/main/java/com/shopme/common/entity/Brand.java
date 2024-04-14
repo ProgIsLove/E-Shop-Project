9d1a5520d531dafc5658d3a672adaf62604179d1
Tomáš Pardygal <58989540+ProgIsLove@users.noreply.github.com>
2024-04-14 21:26:10 +0200

    brand entity (#41)

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -0,0 +1,31 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 45, unique = true)
+    private String name;
+    @Column(nullable = false, length = 128)
+    private String logo;
+
+    @ManyToMany
+    @JoinTable(name = "brand_categories",
+    joinColumns = @JoinColumn(name = "brand_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+}
