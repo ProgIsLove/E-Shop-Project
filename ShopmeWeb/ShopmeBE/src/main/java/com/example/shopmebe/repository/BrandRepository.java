8674515a099fedff91531708b282a18c291fc578
Tomáš <tomas.pardygal@seznam.cz>
2024-05-30 22:23:54 +0200

    check unique brand name

--- ShopmeWeb/ShopmeBE/src/main/java/com/example/shopmebe/repository/BrandRepository.java
+++ ShopmeWeb/ShopmeBE/src/main/java/com/example/shopmebe/repository/BrandRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer>, CrudRepository<Brand, Integer> {
 
     Long countById(Integer id);
+
+    Brand findByName(String name);
 }
