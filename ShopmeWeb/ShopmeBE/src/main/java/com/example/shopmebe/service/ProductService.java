a96c76cdb8c2c7465640a1a1c8f59364f31f3157
Tomáš <tomas.pardygal@seznam.cz>
2024-06-23 14:29:44 +0200

    product save

--- ShopmeWeb/ShopmeBE/src/main/java/com/example/shopmebe/service/ProductService.java
+++ ShopmeWeb/ShopmeBE/src/main/java/com/example/shopmebe/service/ProductService.java
@@ -4,6 +4,7 @@ import com.example.shopmebe.repository.ProductRepository;
 import com.shopme.common.entity.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -17,5 +18,22 @@ public class ProductService {
 
     public List<Product> listAll() {
         return (List<Product>) productRepository.findAll();
+    }
+
+    public void save(Product product) {
+        if (product.getId() == null) {
+            product.setCreatedTime(new Date());
+        }
+
+        if (product.getAlias() == null || product.getAlias().isEmpty()) {
+            String defaultAlias = product.getName().replaceAll(" ", "-");
+            product.setAlias(defaultAlias);
+        } else {
+            product.setAlias(product.getAlias().replaceAll(" ", "-"));
+        }
+
+        product.setModifiedTime(new Date());
+
+        productRepository.save(product);
     }
 }
