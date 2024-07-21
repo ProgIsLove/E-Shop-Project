9710406c45333c4a1f0522a2b42029a1ab5106aa
Tomáš Pardygal <58989540+ProgIsLove@users.noreply.github.com>
2024-07-21 18:17:31 +0200

    added details on product form (#75)

---
+++ ShopmeWeb/ShopmeBE/src/main/resources/static/js/product_form_details.js
@@ -0,0 +1,32 @@
+function addNextDetailSection() {
+    let allDivDetails = $("[id^='divDetail']");
+    let divDetailsCount = allDivDetails.length;
+
+    let htmlDetailSection = `<div class="row col-sm-8 mb-3 mt-3" id="divDetail${divDetailsCount}">
+        <label for="detailNames" class="col-sm-1 col-form-label">Name:</label>
+        <div class="col-sm-3">
+            <input type="text" class="form-control" name="detailNames" maxlength="255" id="detailNames">
+        </div>
+        <label for="detailValues" class="col-sm-1 col-form-label">Value:</label>
+        <div class="col-sm-3">
+            <input type="text" class="form-control" name="detailValues" maxlength="255" id="detailValues">
+        </div>
+    </div>`;
+
+    $(`#divProductDetails`).append(htmlDetailSection);
+
+    let previousDivDetailSection = allDivDetails.last();
+    let previousDivDetailID = previousDivDetailSection.attr(`id`);
+
+    let htmlLinkRemove = `
+        <a class="btn fa fa-times-circle fa-2x icon-dark col-sm-1" 
+        href="javascript:removeDetailSectionById('${previousDivDetailID}')"
+        title="Remove this detail"></a>
+    `;
+
+    previousDivDetailSection.append(htmlLinkRemove);
+}
+
+function removeDetailSectionById(id) {
+    $("#" + id).remove();
+}
---
+++ ShopmeWeb/ShopmeBE/src/main/resources/static/js/product_form_overview.js
@@ -0,0 +1,93 @@
+const DROP_DOWN_BRANDS = $("#brand");
+const DROP_DOWN_CATEGORIES = $("#category");
+
+
+$(document).ready(function () {
+
+    $("#shortDescription").richText();
+    $("#fullDescription").richText();
+
+
+    DROP_DOWN_BRANDS.change(function () {
+        DROP_DOWN_CATEGORIES.empty();
+        getCategories();
+    });
+    //categories to show the categories of the first brand
+    getCategories();
+});
+
+function getCategories() {
+    const brandId = DROP_DOWN_BRANDS.val();
+    const url = `${BRAND_MODULE_URL}/${brandId}/categories`;
+
+    $.get(url, (responseJson) => {
+        responseJson.forEach((category) => {
+            $("<option>")
+                .val(category.id)
+                .text(category.name)
+                .appendTo(DROP_DOWN_CATEGORIES);
+        });
+    });
+}
+
+function checkUnique(form) {
+    let requestData = {
+        id: $("#id").val(),
+        name: $("#name").val().trim(),
+    };
+
+    let csrfToken = $("input[name='_csrf']").val();
+
+    $.post({
+        url: CHECK_UNIQUE_PRODUCT,
+        contentType: "application/json",
+        data: JSON.stringify(requestData),
+        dataTYpe: "json",
+        headers: {
+            "X-CSRF-TOKEN": csrfToken
+        },
+        success: function (response) {
+            switch (response.status) {
+                case "OK":
+                    form.submit();
+                    break;
+                default:
+                    showErrorModal(`Unknown response from server`);
+                    break;
+            }
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status === 409) {
+                let response = JSON.parse(xhr.responseText);
+                showWarningModal(response.details);
+            } else {
+                showErrorModal(`Could not connect to the sever`);
+            }
+        }
+    });
+
+    return false;
+}
+
+$(document).ready(function() {
+    $('form').on('submit', function() {
+        return checkUnique(this);
+    });
+});
+
+function showModalDialog(title, message) {
+    $('#modalTitle').text(title);
+    $('#modalBody').text(message);
+    $('#modalDialog').show();
+    $('#modalDialogClose, #modalDialogCross').on('click', function () {
+        $('#modalDialog').hide();
+    })
+}
+
+function showErrorModal(message) {
+    showModalDialog("Error", message);
+}
+
+function showWarningModal(message) {
+    showModalDialog("Warning", message)
+}
