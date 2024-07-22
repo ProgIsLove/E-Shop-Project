71f3c75f774e06f3cc7831119a7ff8f903bb7be7
Tomáš Pardygal <58989540+ProgIsLove@users.noreply.github.com>
2024-07-22 22:35:07 +0200

    added edit product function (#77)

--- ShopmeCommon/src/main/java/com/shopme/common/entity/ProductImage.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/ProductImage.java
@@ -28,4 +28,9 @@ public class ProductImage {
         this.name = name;
         this.product = product;
     }
+
+    @Transient
+    public String getImagePath() {
+        return "/product-images/" + product.getId() + "/extras/" + this.name;
+    }
 }
