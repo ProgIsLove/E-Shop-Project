6fcf7f693296869c6813f2dbe8c7663bbcf73651
Tomáš Pardygal <58989540+ProgIsLove@users.noreply.github.com>
2024-08-22 22:30:14 +0200

    added currency entity (#93)

---
+++ ShopmeWeb/ShopmeBE/src/main/java/com/example/shopmebe/setting/CurrencyRepository.java
@@ -0,0 +1,7 @@
+package com.example.shopmebe.setting;
+
+import com.shopme.common.entity.Currency;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrencyRepository extends CrudRepository<Currency, Integer> {
+}
