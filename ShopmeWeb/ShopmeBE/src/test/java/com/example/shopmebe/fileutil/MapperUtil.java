dafdd0347fb8452e307d3caec5c80324e2b4469a
Tomáš Pardygal <58989540+ProgIsLove@users.noreply.github.com>
2024-09-09 22:35:33 +0200

    added delete country test, removed unnecessary mapper method (#98)

--- ShopmeWeb/ShopmeBE/src/test/java/com/example/shopmebe/fileutil/MapperUtil.java
+++ ShopmeWeb/ShopmeBE/src/test/java/com/example/shopmebe/fileutil/MapperUtil.java
@@ -1,14 +1,9 @@
 package com.example.shopmebe.fileutil;
 
-import com.example.shopmebe.exception.ApiError;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shopme.common.entity.Country;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
 
 public class MapperUtil {
 
@@ -21,9 +16,5 @@ public class MapperUtil {
 
     public static Country deserializeCountry(String json) throws JsonProcessingException {
         return objectMapper.readValue(json, Country.class);
-    }
-
-    public static ApiError deserializeErrorMsg(String json) throws IOException {
-        return objectMapper.readValue(json, ApiError.class);
     }
 }
